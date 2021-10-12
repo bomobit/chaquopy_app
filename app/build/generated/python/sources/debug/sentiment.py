0aefa71be8b0fb35189b35c38ea827a2cb1d2114
matedomonkos <matedomonkos@gmail.com>
2021-10-12 18:30:22 +0200

    - chaquopy added correctly - basic working method tested - nice

---
+++ app/build/generated/python/sources/debug/sentiment.py
@@ -0,0 +1,2 @@
+def writeOut():
+    return "hello world"
--- app/src/main/java/com/example/sentimentalism/MainActivity.java
+++ app/src/main/java/com/example/sentimentalism/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.sentimentalism;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview = (TextView) findViewById(R.id.textview);
+        Python py = Python.getInstance();
+        PyObject pyobj = py.getModule("sentiment");
+        PyObject obj = pyobj.callAttr("writeOut");
+        textview.setText(obj.toString());
     }
+
 }
